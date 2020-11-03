564deadb2cd972b480742228f3aaa7f649e83d8d
kj <agassi77@gmail.com>
2020-11-03 14:24:23 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.getElementById('number');
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+  console.log(count, action.type);
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  }
   return count;
 };
 
 const countStore = createStore(countModifier);
+
+console.log(countStore.getState());
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
 console.log(countStore.getState());
